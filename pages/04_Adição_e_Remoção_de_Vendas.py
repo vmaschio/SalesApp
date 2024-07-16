b230a4055b58d68d81058406cb11224db66fa566
unknown <victormaschio12@gmail.com>
2024-07-16 17:25:45 -0300

    page 4 changes

--- pages/04_Adição_e_Remoção_de_Vendas.py
+++ pages/04_Adição_e_Remoção_de_Vendas.py
@@ -42,6 +42,49 @@ if adicionar_venda:
     caminho_datasets = st.session_state['caminho_datasets']
     df_vendas.to_csv(caminho_datasets / 'vendas.csv', decimal=',', sep=';')
 
+st.sidebar.markdown('## Edição de Vendas')
+id_edicao = st.sidebar.number_input('Id venda a ser editado:', 0, df_vendas['id_venda'].max(), key='edit_id')
+iniciar_edicao = st.sidebar.button('Editar Venda', key='edit_button')
+
+if iniciar_edicao or 'editando' in st.session_state:
+    if iniciar_edicao:
+        st.session_state.editando = True
+        st.session_state.id_edicao = id_edicao
+        venda_edicao = df_vendas[df_vendas['id_venda'] == id_edicao]
+        st.session_state.filial_selecionada_ed = venda_edicao.iloc[0]['filial'] + '/' + df_filiais.loc[df_filiais['cidade'] == venda_edicao.iloc[0]['filial']]['estado'].values[0]
+        st.session_state.vendedor_selecionado_ed = venda_edicao.iloc[0]['vendedor']
+        st.session_state.produto_selecionado_ed = venda_edicao.iloc[0]['produto']
+        st.session_state.nome_cliente_ed = venda_edicao.iloc[0]['cliente_nome']
+        st.session_state.genero_selecionado_ed = venda_edicao.iloc[0]['cliente_genero']
+        st.session_state.forma_pagamento_ed = venda_edicao.iloc[0]['forma_pagamento']
+    else:
+        id_edicao = st.session_state.id_edicao
+
+    filial_selecionada_ed = st.sidebar.selectbox('Selecione a filial', cidades_filiais, 
+                                                 index=cidades_filiais.index(st.session_state.filial_selecionada_ed), key='edit_filial')
+    vendedores_ed = df_filiais.loc[df_filiais['cidade/estado'] == filial_selecionada_ed, 'vendedores'].iloc[0]
+    vendedores_ed = vendedores_ed.strip('][').replace("'", '').split(', ')
+    vendedor_selecionado_ed = st.sidebar.selectbox('Selecione o vendedor:', vendedores_ed, index=vendedores_ed.index(st.session_state.vendedor_selecionado_ed) if st.session_state.vendedor_selecionado_ed in vendedores_ed else 0, key='edit_vendedor')
+    produto_selecionado_ed = st.sidebar.selectbox('Selecione o produto:', produtos, index=produtos.index(st.session_state.produto_selecionado_ed), key='edit_produto')
+    nome_cliente_ed = st.sidebar.text_input('Nome do cliente', value=st.session_state.nome_cliente_ed, key='edit_nome_cliente')
+    genero_selecionado_ed = st.sidebar.selectbox('Gênero do cliente:', ['masculino', 'feminino'], index=['masculino', 'feminino'].index(st.session_state.genero_selecionado_ed), key='edit_genero')
+    forma_pagamento_ed = st.sidebar.selectbox('Forma de pagamento:', ['boleto', 'pix', 'credito'], index=['boleto', 'pix', 'credito'].index(st.session_state.forma_pagamento_ed), key='edit_forma_pagamento')
+    
+    confirmar_edicao = st.sidebar.button('Confirmar Edição', key='confirm_edit_button')
+    if confirmar_edicao:
+        df_vendas.loc[df_vendas['id_venda'] == id_edicao, ['filial', 'vendedor', 'produto', 'cliente_nome', 'cliente_genero', 'forma_pagamento']] = [
+            filial_selecionada_ed.split('/')[0],
+            vendedor_selecionado_ed,
+            produto_selecionado_ed,
+            nome_cliente_ed,
+            genero_selecionado_ed,
+            forma_pagamento_ed
+        ]
+        caminho_datasets = st.session_state['caminho_datasets']
+        df_vendas.to_csv(caminho_datasets / 'vendas.csv', decimal=',', sep=';', index=False)
+        st.session_state['dados']['df_vendas'] = df_vendas
+        st.session_state.editando = False
+
 st.sidebar.markdown('## Remoção de Vendas')
 id_remocao = st.sidebar.number_input('Id venda a ser removido:',
                                      0,
