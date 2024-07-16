8c2a684d19b531c178ffcaca035fa557e161388d
VICTOR MASCHIO <152653542+vmaschio@users.noreply.github.com>
2024-07-16 13:33:54 -0300

    Add files via upload

--- utilidades.py
+++ utilidades.py
@@ -13,3 +13,5 @@ def leitura_de_dados():
             'df_produtos': df_produtos}
         st.session_state['caminho_datasets'] = path_dataset
         st.session_state['dados'] = dados
+
+comissao = 0.08
